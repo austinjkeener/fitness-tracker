58a085cfa07bf6a4502b87011abaee568970b0be
Austin Keener <austingraphicandweb@gmail.com>
2020-07-26 19:42:30 -0400

    addition of initial files

---
+++ Develop/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
+
+app.get("/", (req, res) => {
+  db.Note.find({})
+    .then(dbNote => {
+      res.json(dbNote);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/exercise", (req, res) => {
+  db.Exercise.find({})
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/stats", (req, res) => {
+  db.Stats.find({})
+    .then(dbStats => {
+      res.json(dbStats);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+// app.post("/", ({ body }, res) => {
+//   db.Note.create(body)
+//     .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
+//     .then(dbUser => {
+//       res.json(dbUser);
+//     })
+//     .catch(err => {
+//       res.json(err);
+//     });
+// });
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
